5e9b39a72d388c37348c803d6b0875b6b0a928df
denis <denis.kharchenko1@gmail.com>
2021-07-06 17:02:41 +0700

    add style

---
+++ src/reducers/index.js
@@ -0,0 +1,93 @@
+
+const initialState = {
+  books: [],
+  loading: true,
+  error: null,
+  cartItems: [],
+  orderTotal: 220
+};
+
+const updateCartItems = (cartItems, item, idx) => {
+  if (idx === -1) {
+    return [
+      ...cartItems,
+      item
+    ];
+  }
+  return [
+    ...cartItems.slice(0, idx),
+    item,
+    ...cartItems.slice(idx + 1)
+  ];
+};
+
+
+
+const updateCartItem = (book, item) => {
+
+  if (item) {
+    return {
+      ...item,
+      count: item.count + 1,
+      total: item.total + book.price
+    };
+  } else {
+    return {
+      id: book.id,
+      title: book.title,
+      count: 1,
+      total: book.price
+    };
+  }
+}
+
+const reducer = (state = initialState, action) => {
+
+  switch (action.type) {
+    case 'FETCH_BOOKS_REQUEST':
+      return {
+        ...state,
+        books: [],
+        loading: true,
+        error: null
+      };
+
+    case 'FETCH_BOOKS_SUCCESS':
+      return {
+        ...state,
+        books: action.payload,
+        loading: false,
+        error: null
+      };
+
+    case 'FETCH_BOOKS_FAILURE':
+      return {
+        ...state,
+        books: [],
+        loading: false,
+        error: action.payload
+      };
+
+    case 'BOOK_ADDED_TO_CART':
+      const bookId = action.payload;
+      const book = state.books.find((book) => book.id === bookId);
+      const itemIndex = state.cartItems.findIndex(({id}) => id === bookId);
+      const item = state.cartItems[itemIndex];
+
+      
+
+      const newItem = updateCartItem(book, item);
+      return {
+        ...state,
+        cartItems: updateCartItems(state.cartItems, newItem, itemIndex)
+      };
+      
+
+      
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
